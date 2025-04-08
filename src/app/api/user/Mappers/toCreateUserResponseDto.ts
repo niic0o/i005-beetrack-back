71114de5972c830a7a298c1a1e6ffb30d747476f
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-04-08 12:54:10 -0300

    feat: agrega archivo para mapear User (prisma) a createUserResponseDto

---
+++ src/app/api/user/Mappers/toCreateUserResponseDto.ts
@@ -0,0 +1,14 @@
+import { User } from '@prisma/client';
+import { createUserResponseDto } from '../DTOs/createUserResponseDto';
+
+export const toCreateUserResponseDto = (user: User) => {
+  return createUserResponseDto.parse({
+    id: user.id,
+    name: user.name,
+    last_name: user.last_name,
+    email: user.email,
+    birthdate: user.birthdate.toISOString(),
+    createdAt: user.createdAt.toISOString(),
+    updatedAt: user.updatedAt.toISOString(),
+  });
+};
