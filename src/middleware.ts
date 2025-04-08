bd56e837fefb9539bea3bffa33ac4e880c5daa9f
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-04-08 12:45:52 -0300

    feat: agrega middleware para configurar cors
    Separo el middleware de autenticación en su propio archivo para modularizar. Luego importo ambos (auth y cors) al archivo middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,49 +1,23 @@
-/*
-  Este middleware extrae el token contenido dentro de una cookie en la aplicacion del cliente
-  Lo hace cada vez que el cliente quiere acceder a una ruta protegida de nuestra api
-  Si el token no existe, redirecciona a la pagina de login.
-  Si el token existe, comprueba que sea valido (que lo consiguió a traves de un login legitimo)
-  Si es valido continua con el acceso, si no responde con un error 401 Unauthorized
+import { NextRequest, NextResponse } from 'next/server';
+import { corsMiddleware } from '@/middleware/cors';
+import { authMiddleware } from '@/middleware/auth';
 
-*/
+export function middleware(req: NextRequest) {
+  const corsResponse = corsMiddleware(req);
+  if (corsResponse) return corsResponse;
 
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { jwtVerify } from "jose";
+  //autenticación para todas las rutas excepto login y register
+  const excludedPaths = ['/api/auth/login', '/api/auth/register'];
 
-export const config = {
-  // Ejecuta el middleware para todos menos:
-  matcher: ["/((?!api/auth/login|_next/static|_next/image|favicon.ico).*)"],
-};
-
-const secret_key = new TextEncoder().encode(process.env.SECRET_KEY);
-
-export async function middleware(request: NextRequest) {
-  const jwt = request.cookies.get("token")?.value;
-
-  if (!jwt) {
-    // Cuando tengamos front esto redirige a la página del login:
-    // return NextResponse.redirect(new URL("/auth/login", request.url));
-    return NextResponse.json(
-      { message: "No ha iniciado sesión" },
-      { status: 401 }
-    );
-  }
-
-  try {
-    const { payload } = await jwtVerify(jwt, secret_key);
-    // Podrías tipar `payload` si querés más seguridad
-  } catch (err: unknown) {
-    const errorMessage =
-      err instanceof Error ? err.message : "Error desconocido";
-
-    console.error("Error al verificar el token:", errorMessage);
-
-    return NextResponse.json(
-      { message: "Token inválido o expirado" },
-      { status: 401 }
-    );
+  if (!excludedPaths.includes(req.nextUrl.pathname)) {
+    const authResponse = authMiddleware(req);
+    if (authResponse) return authResponse;
   }
 
   return NextResponse.next();
 }
+
+//Se aplica el middleware a todas las rutas de la carpeta api
+export const config = {
+  matcher: '/api/:path*',
+};
