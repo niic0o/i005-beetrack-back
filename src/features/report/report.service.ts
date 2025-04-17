1ba2e5e7a12ace6152521622258f33852bb80307
niic0o <nicosena10@gmail.com>
2025-04-16 22:33:06 -0300

    añade comentario a la funcion

--- src/features/report/report.service.ts
+++ src/features/report/report.service.ts
@@ -2,6 +2,8 @@ import { prisma } from "@/lib/prisma";
 
 /**
  * Ejecuta la función SQL cerrar_caja_diaria() y devuelve la cantidad de reportes generados
+ * Esa funcion es una consulta SQL que recuenta todas las ordenes de venta del dia actual
+ * Y genera los reportes diarios para todas las tiendas
  */
 export async function ejecutarCierreCajaSQL(): Promise<number> {
   const result = await prisma.$queryRaw<{ cerrar_caja_diaria: number }[]>`
