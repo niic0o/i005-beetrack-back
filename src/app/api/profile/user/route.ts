45cab8c5f0a5c49d2bb4d6c1214557bee63f928e
niic0o <nicosena10@gmail.com>
2025-04-19 12:43:40 -0300

    fix-javi: [endpoint PATCH] profile/user/route

---
+++ src/app/api/profile/user/route.ts
@@ -0,0 +1,41 @@
+import { getTokenFromCookie } from "@/lib/getTokenFromCookie";
+import { ResourceNotFound, UnauthorizedError } from "@/lib/errors/customErrors";
+import { getUserFromToken } from "@/lib/getUserFromToken";
+import { handleError } from "@/lib/errors/errorHandler";
+import { getUserProfile, updateStore } from "@/features/users/user.service";
+import { successResponse } from "@/lib/responses";
+import { ProfileData } from "@/features/users/user.dto";
+
+
+export async function PATCH(req: Request) {
+  try {
+    const token = getTokenFromCookie(req);
+
+    // ?: Se está validando la validez del token?
+    if (!token) {
+      throw new UnauthorizedError('Token inválido o faltante');
+    }
+    const user = await getUserFromToken(token);
+    if (!user) {
+      throw new ResourceNotFound('Usuario no encontrado');
+    }
+
+    const body = await req.json();
+
+    const profile = await getUserProfile(user.userId, user.storeId);
+
+    if(!profile || !profile.store) {
+      throw new ResourceNotFound('Usuario no encontrado');
+    }
+
+    const { store: storeEntity } = profile;
+
+    const updatedStore = await updateStore(storeEntity, body);
+    
+    const updatedProfile: ProfileData = { ...profile, store: updatedStore }
+
+    return successResponse(updatedProfile);
+  } catch (error) {
+    return handleError(error);
+  }
+}
