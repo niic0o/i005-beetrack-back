716bba6e4bd0cd2e93aa5a0634c7f58acd963663
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-04-19 15:02:44 -0300

    feat: :sparkles: agrega servicio y controlador para crear una orden

---
+++ src/app/api/orders/route.ts
@@ -0,0 +1,47 @@
+import { createOrderRequestDto } from '@/features/orders/DTOs/createOrderRequestDto';
+import { createOrder } from '@/features/orders/order.service';
+import {
+  ResourceNotFound,
+  UnauthorizedError,
+  ValidationError,
+} from '@/lib/errors/customErrors';
+import { handleError } from '@/lib/errors/errorHandler';
+import { getTokenFromCookie } from '@/lib/getTokenFromCookie';
+import { getUserFromToken } from '@/lib/getUserFromToken';
+import { successResponse } from '@/lib/responses';
+import { NextResponse } from 'next/server';
+
+export async function POST(req: Request) {
+  try {
+    const token = getTokenFromCookie(req);
+    if (!token) {
+      throw new UnauthorizedError('Token inválido o faltante');
+    }
+    const user = await getUserFromToken(token);
+    if (!user) {
+      throw new ResourceNotFound('Usuario no encontrado');
+    }
+
+    const orderData = await req.json();
+    if (!orderData) {
+      throw new ValidationError(
+        'No se han enviado campos en el cuerpo de la solicitud'
+      );
+    }
+    orderData.storeId = user.storeId;
+    const validatedData = createOrderRequestDto.parse(orderData);
+    const serviceResponse = await createOrder(validatedData);
+    return successResponse(serviceResponse);
+  } catch (error) {
+    if (error instanceof Array) {
+      return NextResponse.json(
+        {
+          status: 'fail',
+          errors: error,
+        },
+        { status: 400 }
+      );
+    }
+    return handleError(error);
+  }
+}
