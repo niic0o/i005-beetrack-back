5dc3ab3329afb672acda5aed9792fef555bfc1e8
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-04-21 18:32:31 -0300

    agrega validaciones a barcode y stocks

--- src/features/products/DTOs/createProductRequestDto.ts
+++ src/features/products/DTOs/createProductRequestDto.ts
@@ -3,8 +3,14 @@ import { ProductStatus } from '@prisma/client';
 import { zodEnumFromPrisma } from '@/lib/zodEnumFromPrisma';
 
 export const createProductRequestDto = z.object({
-  barcode: z.string().nonempty(),
-  name: z.string().nonempty(),
+  barcode: z.string().regex(/^[0-9-]{8,16}$/, {
+    message:
+      'Debe tener entre 8 y 16 caracteres y solo puede contener números y guiones medios',
+  }),
+  name: z
+    .string()
+    .nonempty()
+    .max(50, { message: 'Debe contener un máximo de 50 caracteres' }),
   alerts: z
     .preprocess((val) => {
       if (val === 'true') return true;
@@ -15,12 +21,21 @@ export const createProductRequestDto = z.object({
   storeId: z.string().nonempty(),
   salesPrice: z.coerce.number().positive(),
   costPrice: z.coerce.number().positive(),
-  stock: z.coerce.number().int().nonnegative(),
-  stock_optimus: z.coerce.number().int().nonnegative(),
-  stock_min: z.coerce.number().int().nonnegative(),
-  imagePath: z.string(),
-  cloudinary_id: z.string(),
-  description: z.string(),
+  stock: z.coerce
+    .number()
+    .int()
+    .min(1, { message: 'El stock mínimo para registrar un producto es 1' }),
+  stock_optimus: z.coerce
+    .number()
+    .int()
+    .min(1, { message: 'El stock_optimus mínimo es 1' }),
+  stock_min: z.coerce
+    .number()
+    .int()
+    .min(1, { message: 'El stock_min mínimo es 1' }),
+  imagePath: z.string().max(255),
+  cloudinary_id: z.string().max(255),
+  description: z.string().max(255),
   status: zodEnumFromPrisma(ProductStatus),
 });
 
--- src/features/products/DTOs/updateProductRequestDto.ts
+++ src/features/products/DTOs/updateProductRequestDto.ts
@@ -2,7 +2,8 @@ import { z } from 'zod';
 import { createProductRequestDto } from './createProductRequestDto';
 
 export const updateProductRequestDto = createProductRequestDto
-  .omit({ barcode: true })
+  .omit({ barcode: true, stock: true })
+  .extend({ stock: z.coerce.number().nonnegative() })
   .partial();
 
 export type UpdateProductRequestDtoType = z.infer<
